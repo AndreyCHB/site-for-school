8a8c9a80d30be1e68222da8abbfdb7bcc5f9100f
Andrey <andreychb@mail.ru>
2021-03-19 18:20:08 +0300

    Update

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -6,6 +6,7 @@ export const LinkCard = ({ link }) => {
             <h2>Вес сегодня</h2>
             <p>Вес: <a>{link.from}</a></p>
             <p>Дата создания: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
+            <button onClick={LinksPage}>Статистика</button>
         </>
     )
 }

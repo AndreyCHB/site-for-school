c74fa19bf3a6c5d0333db0f69c4af5e157ff62b3
Andrey <andreychb@mail.ru>
2021-03-19 18:05:14 +0300

    Update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -22,7 +22,7 @@ router.post(
             if (!errors.isEmpty()) {
                 return res.status(400).json({
                     errors: errors.array(),
-                    message: 'Некорректный данные при регистрации'
+                    message: 'Некорректные данные при регистрации'
                 })
             }
 
@@ -60,7 +60,7 @@ router.post(
             if (!errors.isEmpty()) {
                 return res.status(400).json({
                     errors: errors.array(),
-                    message: 'Некорректный данные при входе в систему'
+                    message: 'Некорректные данные при входе в систему'
                 })
             }
 
